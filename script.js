179918946c89bce73d5dfed3f7c65462ff3fd94c
sahagouran <154049900+sahagouran@users.noreply.github.com>
2024-05-17 10:16:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('.toggle-button');
-    const menu = document.querySelector('.menu');
-
-    toggleButton.addEventListener('click', function() {
-        toggleButton.classList.toggle('active');
-        menu.classList.toggle('active');
-    });
-});
